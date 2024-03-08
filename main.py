602d81791d2a07a982f2df98b3b7236fc7f25472
Hector <iocolopus@gmail.com>
2024-03-09 00:58:56 +0100

    primera version funcional antes de pasar testes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 from math import ceil
-import pprint
+from pprint import pprint
 from copy import deepcopy
 
 Y = 0
@@ -19,10 +19,11 @@ board = [
             [0, 0, 0, 7, 0, 5, 0, 0, 0],
             [1, 0, 0, 0, 4, 0, 0, 0, 7]
         ]
-board = np.array(board)
 
 
 def posible_numbers(sudoku, pos):
+
+    sudoku = np.array(sudoku)
 
     set_row = set(sudoku[pos[Y]]) - {0}
     set_col = set([row[pos[X]] for row in sudoku]) - {0}
@@ -35,20 +36,29 @@ def posible_numbers(sudoku, pos):
     return {1, 2, 3, 4, 5, 6, 7, 8, 9} - set_square - set_row - set_col
 
 def sudoku_recursivo(sudoku):
+    global terminado
 
     sudoku_copia = deepcopy(sudoku)
 
-    for i in len(board):
-        for j in len(board[0]):
-            if board[i][j] == 0:
-                numbers = posible_numbers(board, (i, j))
-                if numbers == set():
-                    return False
+    for i in range(len(sudoku_copia)):
+        for j in range(len(sudoku_copia[0])):
+            if sudoku_copia[i][j] == 0:
+                numeros = posible_numbers(sudoku_copia, (i, j))
+                if numeros == set():
+                    return None
                 else:
-                    for number in numbers:
-                        sudoku_copia[i][j] = number
+                    for numero in numeros:
+                        sudoku_copia[i][j] = numero
+                        aux = sudoku_recursivo(sudoku_copia)
+                        if aux == None:
+                            continue
+                        return aux
+                    return None
+
+    return sudoku_copia
 
 
 
 
-sudoku_recursivo(board)
+board = sudoku_recursivo(board)
+pprint(board)
